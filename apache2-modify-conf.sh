5ada6f1eed0fb2b5650056668d1637ed3e8e770d
WJ Carey <wjcarey_personal@pop-os.localdomain>
2020-10-01 18:48:21 -0600

    better commandline echos

--- apache2-modify-conf.sh
+++ apache2-modify-conf.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 #APACHE2 & PHP MODIFY CONFIG
-echo "enabling rewrite, expires, headers modules to apache ..."
+echo "enabling rewrite, expires, and headers modules to apache ..."
 a2enmod rewrite expires headers
-echo "enabling rewrite rules to apache directory ..."
-sed -i '0,/AllowOverride None/s//AllowOverride All/; 2,/AllowOverride None/s//AllowOverride All/; 0,/Require all denied/s//Require all granted/' /etc/apache2/apache2.conf
+echo "updating rewrite rules to apache config ..."
+sed -i '0,/AllowOverride None/s//AllowOverride All/; 3,/AllowOverride None/s//AllowOverride All/; 0,/Require all denied/s//Require all granted/' /etc/apache2/apache2.conf
 echo "silencing apache server tokens from server ..."
 printf "\n#Remove Server Tokens\nServerTokens Prod\n#Remove server signature\nServerSignature Off" >> /etc/apache2/apache2.conf
-echo "updating php.ini for higher memory and upload size ..."
+echo "updating php config with higher memory and upload size ..."
 sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 1G/g;s/post_max_size = 8M/post_max_size = 1G/g;s/memory_limit = 128M/memory_limit = 512M/g;s/max_execution_time = 30/max_execution_time = 300/g;s/max_input_time = 60/max_input_time = 300/g;' /etc/php/7.4/apache2/php.ini
 echo "restarting apache2 ..."
 systemctl restart apache2
+echo "success: configuration complete ..."
 
 rm -- "$0"
 exit
